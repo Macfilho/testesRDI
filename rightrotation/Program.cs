d922f898da39b26d6f5369dee4dab2e284b64b8c
85181285104 <mauro.filho@apoioprodes.sp.gov.br>
2022-03-11 09:10:38 -0300

    Adicionar arquivos de projeto.

---
+++ rightrotation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace rightrotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] oneDimensionalArray = new int[3];
+            Console.WriteLine("Enter the one Digit Array Elements : ");
+            for (int i = 0; i < oneDimensionalArray.Length; i++)
+            {
+                oneDimensionalArray[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            int k;
+            for (int j = 0; j < oneDimensionalArray.Length - 1; j++)
+            {
+                k = oneDimensionalArray[0];
+                oneDimensionalArray[0] = oneDimensionalArray[j + 1];
+                oneDimensionalArray[j + 1] = k;
+
+            }
+            Console.WriteLine("Array Elements After Right Circular First Rotation:");
+            foreach (int num in oneDimensionalArray)
+            {
+                // Console.Write( num + " ");
+                Console.WriteLine(num + " ");
+
+
+            }
+
+            k = oneDimensionalArray[2];
+            for (int j = 0; j < oneDimensionalArray.Length - 1; j++)
+            {
+                k = oneDimensionalArray[0];
+                oneDimensionalArray[0] = oneDimensionalArray[j + 1];
+                oneDimensionalArray[j + 1] = k;
+
+            }
+
+            Console.WriteLine("Array Elements After Right Circular Seccond Rotation:");
+            foreach (int num in oneDimensionalArray)
+            {
+                Console.Write(num + " ");
+                //Console.WriteLine(num + " ");
+
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
